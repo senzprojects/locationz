ef7b0cf9202709beb20e81aaacd4a31367d48c0c
eranga herath <erangaeb@gmail.com>
2015-10-21 19:35:36 +0530

    added switch board

---
+++ app/src/main/java/com/score/senz/ui/SenzSwitchBoardActivity.java
@@ -0,0 +1,12 @@
+package com.score.senz.ui;
+
+import android.app.Activity;
+
+/**
+ * Activity class which handler switching
+ *
+ * @author eranga herath(erangaeb@gmail.com)
+ */
+public class SenzSwitchBoardActivity extends Activity {
+
+}
