6b3077486487ec8417d38bf8670a9c9b9099b9ec
eranga herath <erangaeb@gmail.com>
2016-02-01 22:34:06 +0530

    refactored

--- app/src/main/java/com/score/locationz/listeners/LocationAddressReaderListener.java
+++
@@ -1,8 +0,0 @@
-package com.score.locationz.listeners;
-
-/**
- * Created by eranga on 9/23/15.
- */
-public interface LocationAddressReaderListener {
-    public void onPostLocationAddressRead(String address);
-}
